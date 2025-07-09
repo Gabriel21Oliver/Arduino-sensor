a9dac93f3bb3075270ba5778c5fddae73bc396ab
Gabriel Vitor <90521193+Gabriel21Oliver@users.noreply.github.com>
2025-07-09 10:59:46 -0300

    Update semaforo.ino

--- ArduinoSemaforoSensor/semaforo.ino
+++ ArduinoSemaforoSensor/semaforo.ino
@@ -1,36 +1,81 @@
-const int trigPin = 9;
-const int echoPin = 10;
-const int ledVermelho = 3;
-const int ledVerde = 4;
+/******************************************************************************/
+/*          Projeto: Protótipo Sensor para Estacionamento em Garagem          */
+/******************************************************************************/
+
+//Configuração de Distancia Mínima em centimetros
+const int distancia_carro = 10; 
+
+//Configurações de Portas do Arduino
+
+//Sensor
+const int TRIG = 3;
+const int ECHO = 2;
+
+//Demais componentes
+const int ledGreen = 7;
+const int ledRed = 8;
+const int buzzer = 9;
+
+// Variaveis para funcionamento do Buzzer
+float seno;
+int frequencia;
 
 void setup() {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(ledVermelho, OUTPUT);
-  pinMode(ledVerde, OUTPUT);
   Serial.begin(9600);
+  
+  // Configurações do Sensor
+  pinMode(TRIG,OUTPUT);
+  pinMode(ECHO,INPUT);
+  
+  // Configurações do LED
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+
+  //Configurações do Buzzer
+  pinMode(buzzer, OUTPUT); 
+  
 }
 
 void loop() {
-  digitalWrite(trigPin, LOW);
+  int distancia = sensor_morcego(TRIG,ECHO);
+
+    if(distancia <= distancia_carro){
+      Serial.print("Atenção: ");
+      Serial.print(distancia);
+      Serial.println("cm");
+      digitalWrite(ledGreen, LOW);
+      digitalWrite(ledRed, HIGH);
+      tocaBuzzer();
+    }
+    else{
+      Serial.print("Livre: ");
+      Serial.print(distancia);
+      Serial.println("cm");
+      digitalWrite(ledGreen, HIGH);
+      digitalWrite(ledRed, LOW);
+      noTone(buzzer);
+    }
+  delay(100);
+  
+}
+
+int sensor_morcego(int pinotrig,int pinoecho){
+  digitalWrite(pinotrig,LOW);
   delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(pinotrig,HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(pinotrig,LOW);
 
-  long duracao = pulseIn(echoPin, HIGH);
-  float distancia = duracao * 0.034 / 2;
+  return pulseIn(pinoecho,HIGH)/58;
+}
 
-  Serial.print("Distância: ");
-  Serial.println(distancia);
+//Função para execução do Alarme Sonoro
+void tocaBuzzer(){
+ for(int x=0;x<180;x++){
+  seno=(sin(x*3.1416/180));
+  frequencia = 2000+(int(seno*1000));
+  tone(buzzer,frequencia);
+  delay(2);
+}
 
-  if (distancia < 10.0) {
-    digitalWrite(ledVermelho, HIGH);
-    digitalWrite(ledVerde, LOW);
-  } else {
-    digitalWrite(ledVermelho, LOW);
-    digitalWrite(ledVerde, HIGH);
-  }
-
-  delay(200);
 }
